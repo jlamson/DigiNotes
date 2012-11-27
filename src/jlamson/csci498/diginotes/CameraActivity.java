42d2e1928fa6d36ce4148dc4b6376e7e97415c56
Joshua Lamson <jlamson@mymail.mines.edu>
2012-11-27 12:05:53 -0700

    Camera Intent is now started using startActivityForResult(). Added onResultReturned. When photo is taken, raise a toast

--- src/jlamson/csci498/diginotes/CameraActivity.java
+++ src/jlamson/csci498/diginotes/CameraActivity.java
@@ -2,24 +2,35 @@ package jlamson.csci498.diginotes;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class CameraActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
-   
-        Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivity(camera);
-    }
+	private static int CAMERA_REQUEST = 1337;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_camera, menu);
-        return true;
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camera);
+
+		Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(camera, CAMERA_REQUEST);
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+			Toast.makeText(this, "You took a photo", Toast.LENGTH_SHORT)
+				.show();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_camera, menu);
+		return true;
+	}
+
 }
