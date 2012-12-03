802bd2d9ab77794b95efd0f8bf6e606d0899ff14
Joshua Lamson <jlamson@mymail.mines.edu>
2012-12-03 13:44:32 -0700

    added CameraView stub

--- src/jlamson/csci498/diginotes/CameraActivity.java
+++ src/jlamson/csci498/diginotes/CameraActivity.java
@@ -1,41 +1,41 @@
 package jlamson.csci498.diginotes;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 /**
  * This activity runs the camera, and will display the icons representing notes.
  * @author darkmoose117
  *
  */
-public class CameraActivity extends Activity {
-
-	private static int CAMERA_REQUEST = 1337;
+public class CameraActivity extends Activity 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camera);
-
-//		Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-//		startActivityForResult(camera, CAMERA_REQUEST);
 		
 		FrameLayout frame = (FrameLayout)findViewById(R.id.frame);
 		CameraView cv = new CameraView(this);
 		frame.addView(cv);
+	}
+	
+	public class CameraView extends SurfaceView {
+		
+		Camera cam;
+		SurfaceHolder cameraPreview;
+		
+		public CameraView(Context c) {
+			super(c);
+		}
 		
 	}
-
-//	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//		if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
-//			Toast.makeText(this, "You took a photo", Toast.LENGTH_SHORT)
-//				.show();
-//		}
-//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
