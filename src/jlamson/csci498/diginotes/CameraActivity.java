602117e9b0b6e1ed89b011c4423f621ce9d03cfc
Joshua Lamson <jlamson@mymail.mines.edu>
2012-12-03 13:39:52 -0700

    modified onCreate to add a CameraView to the FrameView

--- src/jlamson/csci498/diginotes/CameraActivity.java
+++ src/jlamson/csci498/diginotes/CameraActivity.java
@@ -2,11 +2,16 @@ package jlamson.csci498.diginotes;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+/**
+ * This activity runs the camera, and will display the icons representing notes.
+ * @author darkmoose117
+ *
+ */
 public class CameraActivity extends Activity {
 
 	private static int CAMERA_REQUEST = 1337;
@@ -16,16 +21,21 @@ public class CameraActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camera);
 
-		Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-		startActivityForResult(camera, CAMERA_REQUEST);
+//		Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//		startActivityForResult(camera, CAMERA_REQUEST);
+		
+		FrameLayout frame = (FrameLayout)findViewById(R.id.frame);
+		CameraView cv = new CameraView(this);
+		frame.addView(cv);
+		
 	}
 
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
-			Toast.makeText(this, "You took a photo", Toast.LENGTH_SHORT)
-				.show();
-		}
-	}
+//	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//		if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+//			Toast.makeText(this, "You took a photo", Toast.LENGTH_SHORT)
+//				.show();
+//		}
+//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
