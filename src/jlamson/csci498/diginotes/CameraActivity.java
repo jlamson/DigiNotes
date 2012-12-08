f9616043823660c772dc49175a8b382098a37345
Joshua Lamson <jlamson@mymail.mines.edu>
2012-12-08 12:59:53 -0700

    added SensorListener for digital compass

--- src/jlamson/csci498/diginotes/CameraActivity.java
+++ src/jlamson/csci498/diginotes/CameraActivity.java
@@ -1,14 +1,14 @@
 package jlamson.csci498.diginotes;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.FrameLayout;
@@ -21,7 +21,7 @@ import android.widget.FrameLayout;
 public class CameraActivity extends Activity {
 
 	public static final String DEBUG_TAG = "jlamson.csci498.diginotes.DEBUG_TAG";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -56,12 +56,13 @@ public class CameraActivity extends Activity {
 			cameraPreview.addCallback(surfaceHolderListener);
 		}
 
-		SurfaceHolder.Callback surfaceHolderListener = new SurfaceHolder.Callback() {
-			
+		SurfaceHolder.Callback surfaceHolderListener = 
+		new SurfaceHolder.Callback() {
+
 			public void surfaceCreated(SurfaceHolder holder) {
-				
+
 				cam = Camera.open(0);
-								
+
 				try {
 					cam.setPreviewDisplay(cameraPreview);
 				} catch (Throwable e) {
@@ -71,14 +72,12 @@ public class CameraActivity extends Activity {
 			public void surfaceChanged(SurfaceHolder holder, int format,
 					int width, int height) {
 				Parameters params = cam.getParameters();
-				
-				List<Camera.Size> previewSizes = params.getSupportedPreviewSizes();
-				Camera.Size previewSize = previewSizes.get(0);
-			
+
+				Camera.Size previewSize = getBestPreviewSize(width, height);
 				params.setPreviewSize(previewSize.width, previewSize.height);
 				params.setPictureFormat(ImageFormat.JPEG);
 				cam.setParameters(params);
-				
+
 				cam.startPreview();
 			}
 
@@ -86,14 +85,44 @@ public class CameraActivity extends Activity {
 				cam.stopPreview();
 				cam.release();
 			}
+
+			private Camera.Size getBestPreviewSize(int width, int height) {
+				Camera.Size result = null;
+				Camera.Parameters p = cam.getParameters();
+				for (Camera.Size size : p.getSupportedPreviewSizes()) {
+					if (size.width <= width && size.height <= height) {
+						if (result == null) {
+							result = size;
+						} else {
+							int resultArea = result.width * result.height;
+							int newArea = size.width * size.height;
+
+							if (newArea > resultArea) {
+								result = size;
+							}
+						}
+					}
+				}
+				return result;
+			}
 		};
 
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_camera, menu);
-		return true;
-	}
+	public SensorEventListener listener = new SensorEventListener(){
 
+		float direction;
+		
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			
+		}
+
+		   public void onSensorChanged(SensorEvent evt)
+		   {
+		      float vals[] = evt.values;   
+		      direction = vals[0];
+		   }
+		};
+	
+	
 }
