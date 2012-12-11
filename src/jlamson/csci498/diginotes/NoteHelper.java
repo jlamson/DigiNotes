d2ca8d434def4a786ac36ab06c45ecc23e1e9bbe
Joshua Lamson <jlamson@mymail.mines.edu>
2012-12-11 00:42:44 -0700

    Helper class stub in place

---
+++ src/jlamson/csci498/diginotes/NoteHelper.java
@@ -0,0 +1,27 @@
+package jlamson.csci498.diginotes;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class NoteHelper extends SQLiteOpenHelper {
+
+	public NoteHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
